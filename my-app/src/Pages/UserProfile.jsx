bd67c88b964e17e90d3d9871d1b0e6b011a463e5
Angeline Tran <angelinenhanaitran@gmail.com>
2024-04-01 16:06:37 -0600

    oop

--- my-app/src/Pages/UserProfile.jsx
+++ my-app/src/Pages/UserProfile.jsx
@@ -1,11 +1,19 @@
 import React,{useState,useContext} from 'react';
 import "./UserProfile.css";
+import Axios from 'axios'
 import {Link,useNavigate} from "react-router-dom";
 import {UserContext} from "../Context/usercontext";
+import { useParams } from 'react-router-dom';
+
 function UserProfile(){
+    const { user } = useContext(UserContext);
+    let userType = user?.userType;
+    const { email } = useParams();
+
     return(
         <div>
 
+            
         </div>
     );
 }
