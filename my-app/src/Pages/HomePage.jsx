f41eddb34cda35c9fba6d56aa2629931750646ac
Angeline Tran <angelinenhanaitran@gmail.com>
2024-04-02 23:07:08 -0600

    Added banner

--- my-app/src/Pages/HomePage.jsx
+++ my-app/src/Pages/HomePage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import banner from "../Images/banner.jpg";
+import "./HomePage.css"
+
 
 function HomePage(){
     return(
-        <div className="banner">
-            <img class="banner-img" src={banner} alt={banner} id="I141:2772;1:110" />
-            <div className="banner-container">
-                <div className="banner-title">Welcome to Biblicade</div>
-                <div className="banner-subtitle">Discover the next game you can't get enough of</div>
+        <div>
+            <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/8c7ea0e88fe35e88fb7c7c12adb729bff2689be276d999d9d4e43b5049a89489?apiKey=c7b8aa40bb064f6f8fcb00de2b00394b&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/8c7ea0e88fe35e88fb7c7c12adb729bff2689be276d999d9d4e43b5049a89489?apiKey=c7b8aa40bb064f6f8fcb00de2b00394b&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/8c7ea0e88fe35e88fb7c7c12adb729bff2689be276d999d9d4e43b5049a89489?apiKey=c7b8aa40bb064f6f8fcb00de2b00394b&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/8c7ea0e88fe35e88fb7c7c12adb729bff2689be276d999d9d4e43b5049a89489?apiKey=c7b8aa40bb064f6f8fcb00de2b00394b&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/8c7ea0e88fe35e88fb7c7c12adb729bff2689be276d999d9d4e43b5049a89489?apiKey=c7b8aa40bb064f6f8fcb00de2b00394b&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/8c7ea0e88fe35e88fb7c7c12adb729bff2689be276d999d9d4e43b5049a89489?apiKey=c7b8aa40bb064f6f8fcb00de2b00394b&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/8c7ea0e88fe35e88fb7c7c12adb729bff2689be276d999d9d4e43b5049a89489?apiKey=c7b8aa40bb064f6f8fcb00de2b00394b&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/8c7ea0e88fe35e88fb7c7c12adb729bff2689be276d999d9d4e43b5049a89489?apiKey=c7b8aa40bb064f6f8fcb00de2b00394b&"
+            className="img"
+            alt="banner"
+            />
+            <div className="header">
+                <div className="title">Welcome to Biblicade</div>
+                <div className="subtitle-wrapper">
+                    <div className="subtitle">
+                        Discover your next favourite game
+                    </div>
+                </div>
             </div>
-        </div>
+      </div>
 
     );
 }
