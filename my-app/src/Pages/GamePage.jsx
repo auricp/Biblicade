7906a587560ab01296c2279775d2e0b249af28db
akaforce <gab.rodriguez11@outlook.com>
2024-04-07 04:13:14 -0600

    Finished GamePage logic and CSS

--- my-app/src/Pages/GamePage.jsx
+++ my-app/src/Pages/GamePage.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Axios from "axios";
+import images from '../Components/images.js';
+import Nav from "../Components/navbar";
+import "./GamePage.css";
 
 // Define UserList as a function component
 // copied auric da goat/ angie da slay here thanks bbg
@@ -32,8 +35,51 @@ const GamePage = () => {
 
   return (
     <div>
-      <h1>{gameDetails.title}</h1>
-      <p>Hello World!</p>
+        <Nav />
+    
+
+        <div className="gameDetailsPane">
+            <div className="upperDetails">
+                <img className="gameImage" src={images[gameDetails.title]} alt={gameDetails.title} width={'250px'}/>
+                <div className="upperTexts">
+                    <h1 className="gameTitle">{gameDetails.title}</h1>
+                    <h2 className="gameGenre">{gameDetails.genre}</h2>
+                    <p className="ageRest">For Ages {gameDetails.ageRestriction}+</p>
+                </div>
+                <div className="ratingContainer">
+                    <p className="gameRatingTit">Biblicade Score</p>
+                    <p className="gameRating">{gameDetails.ratingScore}</p>
+                </div>
+            </div>
+
+            <div className="lowerDetails">
+                <div className="leftPane">
+                    <h3>{gameDetails.description}</h3>
+                </div>
+
+                <div className="rightPane">
+                    <h2 className="rightPaneTit">Additional Information</h2>
+                    <div className="releaseDate">                        
+                        <p className="releaseDateBold">Release Date</p>
+                        <p>: {gameDetails.releaseYear}/{gameDetails.releaseMonth}/{gameDetails.releaseDay}</p>        
+                    </div>
+
+                    <div className="developerID">
+                        <p className="developerIDBold">Developer</p>
+                        <p>: {gameDetails.developerID}</p>
+                    </div>
+
+                    <div className="publisherID">
+                        <p className="publisherIDBold">Publisher</p>
+                        <p>: {gameDetails.publisherID}</p>
+                    </div>
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+
     </div>
   );
 };
