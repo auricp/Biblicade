981726a44958b308f10f5377315b562c3766123f
Angeline Tran <angelinenhanaitran@gmail.com>
2024-04-10 16:48:41 -0600

    Added favourites icon

--- my-app/src/Pages/GamePage.jsx
+++ my-app/src/Pages/GamePage.jsx
@@ -4,6 +4,7 @@ import Axios from "axios";
 import images from '../Components/images.js';
 import Nav from "../Components/navbar";
 import "./GamePage.css";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 // Define UserList as a function component
 // copied auric da goat/ angie da slay here thanks bbg
@@ -12,12 +13,21 @@ function GameList() {
     return Axios.get('http://localhost:3001/games').then(response => response.data);
 }
 
+// Define UserList as a function component
+function UserList() {
+    // Fetch user list and return it as a promise
+    return Axios.get('http://localhost:3001/users').then(response => response.data);
+}
+
 const GamePage = () => {
-  const { title } = useParams();
-  const [gameDetails, setGameDetails] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [commentInput, setCommentInput] = useState('');
-  const [comments, setComments] = useState([]);
+    const { title } = useParams();
+    const { email } = useParams();
+    const [user, setUser] = useState(null);
+    const [gameDetails, setGameDetails] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [commentInput, setCommentInput] = useState('');
+    const [comments, setComments] = useState([]);
+    const [favorites, setFavorites] = useState([]); // State to store favorite games
 
   useEffect(() => {
     GameList().then(gameList => {
@@ -43,6 +53,16 @@ const GamePage = () => {
         });
   }, [title]);
 
+  useEffect(() => {
+        // Fetch user list and find the current user
+        UserList().then(userList => {
+            const foundUser = userList.find(u => u.email === email);
+            setUser(foundUser);
+        }).catch(error => {
+            console.error("Error fetching user list:", error);
+        });
+    }, [email]);
+
   const handleCommentInputChange = (event) => {
     setCommentInput(event.target.value);
   };
@@ -63,6 +83,25 @@ const GamePage = () => {
     return <div>Loading... Please Wait</div>;
   }
 
+    const handleAddToFavorites = () => {
+        if (!favorites.includes(gameDetails)) {
+            setFavorites([...favorites, gameDetails]);
+        }
+        // Add clicked class
+        document.querySelector('.favorite-icon').classList.add('clicked');
+    };
+
+    const handleRemoveFromFavorites = () => {
+        const updatedFavorites = favorites.filter(game => game.title !== gameDetails.title);
+        setFavorites(updatedFavorites);
+        // Remove clicked class
+        document.querySelector('.favorite-icon').classList.remove('clicked');
+    };
+
+  
+    const isGameInFavorites = () => {
+        return favorites.some(game => game.title === gameDetails.title);
+    };
 
 
   return (
@@ -75,6 +114,14 @@ const GamePage = () => {
                     <h1 className="gameTitle">{gameDetails.title}</h1>
                     <h2 className="gameGenre">{gameDetails.genre}</h2>
                     <p className="ageRest">For Ages {gameDetails.ageRestriction}+</p>
+                    <div className="favorite-container">
+                        {isGameInFavorites() ? (
+                            <FavoriteIcon className="favorite-icon" onClick={handleRemoveFromFavorites} />
+                        ) : (
+                            <FavoriteIcon className="favorite-icon" onClick={handleAddToFavorites} />
+                        )}
+                        <span className="add-fave">Add to Favourites</span>
+                    </div>
                 </div>
                 <div className="ratingContainer">
                     <p className="gameRatingTit">Biblicade Score</p>
